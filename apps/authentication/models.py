621684c148d5cef897cd9677b2d221690ec76f00
Ihor Harahatyi <ihor.harahatyi@atomcream.com>
2022-12-16 18:28:32 +0100

    code from lesson

---
+++ apps/authentication/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **kwargs):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            **kwargs
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **kwargs):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            is_superuser=True,
+            **kwargs
+        )
+        return user
+
+
+class User(AbstractUser):
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    username = None
+    email = models.EmailField(_('email address'), null=False, unique=True)
