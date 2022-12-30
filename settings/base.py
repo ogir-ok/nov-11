65ce177b86b49d7c49b0098a6ae8ec53d4b2dbf6
Ihor Harahatyi <ihor.harahatyi@atomcream.com>
2022-12-30 19:59:33 +0100

    added ut run

--- settings/base.py
+++ settings/base.py
@@ -94,7 +94,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
-LOGIN_URL = '/auth/login/'
+LOGIN_URL = 'uk/auth/login/'
 AUTH_USER_MODEL = 'authentication.User'
 AUTH_PASSWORD_VALIDATORS = [
     {
