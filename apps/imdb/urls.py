186c648b0b714234d97c4e8fe2f7ae75a506b1e1
Ihor Harahatyi <ihor.harahatyi@atomcream.com>
2023-01-06 19:44:38 +0100

    List movies

--- apps/imdb/urls.py
+++ apps/imdb/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
+from apps.imdb.views import MovieListView
+
 app_name = 'imdb'
 
 
 urlpatterns = [
-
+    path('', MovieListView.as_view())
 ]
--- apps/imdb/views.py
+++ apps/imdb/views.py
@@ -3,3 +3,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView, UpdateView
 
+from apps.imdb.models import Movie
+
+
+class MovieListView(ListView):
+    model = Movie
